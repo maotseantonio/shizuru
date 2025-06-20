394a51db3ba6b77a109f44b13600c1e8f862ef98
maotseantonio <thetzinantonio@gmail.com>
2025-06-20 19:26:01 +0630

    update-gpu-config-and-fish

--- hosts/shizuru/users.nix
+++ hosts/shizuru/users.nix
@@ -12,7 +12,7 @@ in {
   imports = [
     inputs.home-manager.nixosModules.home-manager
     (lib.modules.mkAliasOptionModule ["hm"] ["home-manager" "users" "${username}"]) # gitlab/fazzi
-  ];
+  ]; 
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
@@ -55,39 +55,15 @@ in {
 
     defaultUserShell = pkgs.fish;
   };
+  rum.programs.fish = {
+      enable = true;
+      defaultShell = true;
+  };
   nix.settings.allowed-users = ["${username}"];
   environment.shells = with pkgs; [fish];
   environment.systemPackages = with pkgs; [fzf];
   programs.fish.enable = true;
   programs.fish.interactiveShellInit = ''
     ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
-  '';
-  programs = {
-    # Zsh configuration
-    zsh = {
-      enable = false;
-      enableCompletion = false;
-      ohMyZsh = {
-        enable = false;
-        plugins = ["git"];
-        theme = "xiong-chiamiov-plus";
-      };
-
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-
-      promptInit = ''
-        fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
-
-        #pokemon colorscripts like. Make sure to install krabby package
-        #krabby random --no-mega --no-gmax --no-regional --no-title -s;
-
-        source <(fzf --zsh);
-        HISTFILE=~/.zsh_history;
-        HISTSIZE=10000;
-        SAVEHIST=10000;
-        setopt appendhistory;
-      '';
-    };
-  };
+  ''; 
 }
